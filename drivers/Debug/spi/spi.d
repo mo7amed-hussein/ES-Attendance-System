d9055d6b7f7df3acc13c578aebf69899d376fe51
mohamed hussein <m.hussein1389@gmail.com>
2018-01-23 18:51:17 +0200

    add basic elements of webpage

---
+++ drivers/Debug/spi/spi.d
@@ -0,0 +1,36 @@
+spi/spi.d spi/spi.o spi/spi.o: ../spi/spi.c \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h ../spi/spi.h \
+  ../spi/../uart/softuart.h
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+../spi/spi.h:
+
+../spi/../uart/softuart.h:
