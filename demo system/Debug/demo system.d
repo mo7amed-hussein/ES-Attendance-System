5cc60201ef97e5470da9cb321e16dfa4d9068dc3
mohamed hussein <m.hussein1389@gmail.com>
2018-01-27 20:23:53 +0200

    update page contents

---
+++ demo system/Debug/demo system.d
@@ -0,0 +1,70 @@
+demo system.d demo system.o demo\ system.o: .././demo\ system.c \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  ../../drivers/uart/usart0.h ../../drivers/uart/softuart.h \
+  ../../drivers/wifi/wifi.h ../../drivers/dualultrasonic/dualultrasonic.h \
+  ../../drivers/rfid/rfid.h ../../drivers/rfid/../wifi/wifi.h \
+  ../../drivers/pir/pir.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/stdio.h \
+  e:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h \
+  e:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  e:/winavr-20100110/lib/gcc/../../avr/include/string.h
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/iom328p.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+../../drivers/uart/usart0.h:
+
+../../drivers/uart/softuart.h:
+
+../../drivers/wifi/wifi.h:
+
+../../drivers/dualultrasonic/dualultrasonic.h:
+
+../../drivers/rfid/rfid.h:
+
+../../drivers/rfid/../wifi/wifi.h:
+
+../../drivers/pir/pir.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/stdio.h:
+
+e:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stdarg.h:
+
+e:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+e:/winavr-20100110/lib/gcc/../../avr/include/string.h:
---
+++ drivers/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+dualultrasonic\dualultrasonic.c
+
+pir\pir.c
+
+rfid\rfid.c
+
+spi\spi.c
+
+uart\softuart.c
+
+uart\usart0.c
+
+ultrasonic\ultrasonic.c
+
+wifi\wifi.c
+
